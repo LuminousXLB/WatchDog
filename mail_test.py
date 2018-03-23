278d51fe0309228136d654ee04299a1043ebbe79
Taraxacum <john980118@163.com>
2018-03-23 19:09:26 +0800

    import priv_setting debug

--- mail_test.py
+++ mail_test.py
@@ -4,9 +4,10 @@ import os
 
 if __name__ == '__main__':
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "watchdog.settings")
+
     send_mail(
         '[WatchDog] test email',
-        'just test'
-        'Watch Dog <taraxacum45e9a@aliyun.com>',
-        ['454633705@qq.com']
+        'just test',
+        'taraxacum45e9a@aliyun.com',
+        ['john980118@163.com']
     )
--- watchdog/settings.py
+++ watchdog/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import priv_set
+from watchdog.priv_set import *
+#import priv_set
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,7 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
 
 
